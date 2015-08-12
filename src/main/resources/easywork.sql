ad3c651c35569125dd1322627b36020969205d69
Gavin <xgtxxxx@163.com>
2015-08-12 17:03:28 +0800

    add easywork.sql

---
+++ src/main/resources/easywork.sql
@@ -0,0 +1,262 @@
+/*
+SQLyog 企业版 - MySQL GUI v7.14 
+MySQL - 5.0.22-community : Database - quartzjob
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`quartzjob` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `quartzjob`;
+
+/*Table structure for table `ali_ath4_detail` */
+
+DROP TABLE IF EXISTS `ali_ath4_detail`;
+
+CREATE TABLE `ali_ath4_detail` (
+  `id` int(11) NOT NULL auto_increment,
+  `insertTime` datetime default NULL,
+  `businessMonth` int(11) default NULL,
+  `businessDate` date default NULL,
+  `skillGroup` varchar(50) default NULL,
+  `family` varchar(50) default NULL,
+  `name` varchar(50) default NULL,
+  `subject1` varchar(50) default NULL,
+  `subject2` varchar(50) default NULL,
+  `subject3` varchar(50) default NULL,
+  `subject4` varchar(50) default NULL,
+  `count` int(11) default NULL,
+  `duration` double default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_blob_triggers` */
+
+DROP TABLE IF EXISTS `qrtz_blob_triggers`;
+
+CREATE TABLE `qrtz_blob_triggers` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `BLOB_DATA` blob,
+  PRIMARY KEY  (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  CONSTRAINT `qrtz_blob_triggers_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`) REFERENCES `qrtz_triggers` (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_calendars` */
+
+DROP TABLE IF EXISTS `qrtz_calendars`;
+
+CREATE TABLE `qrtz_calendars` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `CALENDAR_NAME` varchar(200) NOT NULL,
+  `CALENDAR` blob NOT NULL,
+  PRIMARY KEY  (`SCHED_NAME`,`CALENDAR_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_cron_triggers` */
+
+DROP TABLE IF EXISTS `qrtz_cron_triggers`;
+
+CREATE TABLE `qrtz_cron_triggers` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `CRON_EXPRESSION` varchar(200) NOT NULL,
+  `TIME_ZONE_ID` varchar(80) default NULL,
+  PRIMARY KEY  (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  CONSTRAINT `qrtz_cron_triggers_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`) REFERENCES `qrtz_triggers` (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_fired_triggers` */
+
+DROP TABLE IF EXISTS `qrtz_fired_triggers`;
+
+CREATE TABLE `qrtz_fired_triggers` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `ENTRY_ID` varchar(95) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `INSTANCE_NAME` varchar(200) NOT NULL,
+  `FIRED_TIME` bigint(13) NOT NULL,
+  `SCHED_TIME` bigint(13) NOT NULL,
+  `PRIORITY` int(11) NOT NULL,
+  `STATE` varchar(16) NOT NULL,
+  `JOB_NAME` varchar(200) default NULL,
+  `JOB_GROUP` varchar(200) default NULL,
+  `IS_NONCONCURRENT` varchar(1) default NULL,
+  `REQUESTS_RECOVERY` varchar(1) default NULL,
+  PRIMARY KEY  (`SCHED_NAME`,`ENTRY_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_history` */
+
+DROP TABLE IF EXISTS `qrtz_history`;
+
+CREATE TABLE `qrtz_history` (
+  `id` int(11) NOT NULL auto_increment,
+  `jobName` varchar(200) default NULL,
+  `triggerName` varchar(200) default NULL,
+  `previousFireTime` datetime default NULL,
+  `nextFireTime` datetime default NULL,
+  `runTime` datetime default NULL,
+  `refireCount` int(11) default NULL,
+  `message` varchar(1000) default NULL,
+  `success` tinyint(1) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_job_details` */
+
+DROP TABLE IF EXISTS `qrtz_job_details`;
+
+CREATE TABLE `qrtz_job_details` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `JOB_NAME` varchar(200) NOT NULL,
+  `JOB_GROUP` varchar(200) NOT NULL,
+  `DESCRIPTION` varchar(250) default NULL,
+  `JOB_CLASS_NAME` varchar(250) NOT NULL,
+  `IS_DURABLE` varchar(1) NOT NULL,
+  `IS_NONCONCURRENT` varchar(1) NOT NULL,
+  `IS_UPDATE_DATA` varchar(1) NOT NULL,
+  `REQUESTS_RECOVERY` varchar(1) NOT NULL,
+  `JOB_DATA` blob,
+  PRIMARY KEY  (`SCHED_NAME`,`JOB_NAME`,`JOB_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_locks` */
+
+DROP TABLE IF EXISTS `qrtz_locks`;
+
+CREATE TABLE `qrtz_locks` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `LOCK_NAME` varchar(40) NOT NULL,
+  PRIMARY KEY  (`SCHED_NAME`,`LOCK_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_paused_trigger_grps` */
+
+DROP TABLE IF EXISTS `qrtz_paused_trigger_grps`;
+
+CREATE TABLE `qrtz_paused_trigger_grps` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  PRIMARY KEY  (`SCHED_NAME`,`TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_scheduler_state` */
+
+DROP TABLE IF EXISTS `qrtz_scheduler_state`;
+
+CREATE TABLE `qrtz_scheduler_state` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `INSTANCE_NAME` varchar(200) NOT NULL,
+  `LAST_CHECKIN_TIME` bigint(13) NOT NULL,
+  `CHECKIN_INTERVAL` bigint(13) NOT NULL,
+  PRIMARY KEY  (`SCHED_NAME`,`INSTANCE_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_simple_triggers` */
+
+DROP TABLE IF EXISTS `qrtz_simple_triggers`;
+
+CREATE TABLE `qrtz_simple_triggers` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `REPEAT_COUNT` bigint(7) NOT NULL,
+  `REPEAT_INTERVAL` bigint(12) NOT NULL,
+  `TIMES_TRIGGERED` bigint(10) NOT NULL,
+  PRIMARY KEY  (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  CONSTRAINT `qrtz_simple_triggers_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`) REFERENCES `qrtz_triggers` (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_simprop_triggers` */
+
+DROP TABLE IF EXISTS `qrtz_simprop_triggers`;
+
+CREATE TABLE `qrtz_simprop_triggers` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `STR_PROP_1` varchar(512) default NULL,
+  `STR_PROP_2` varchar(512) default NULL,
+  `STR_PROP_3` varchar(512) default NULL,
+  `INT_PROP_1` int(11) default NULL,
+  `INT_PROP_2` int(11) default NULL,
+  `LONG_PROP_1` bigint(20) default NULL,
+  `LONG_PROP_2` bigint(20) default NULL,
+  `DEC_PROP_1` decimal(13,4) default NULL,
+  `DEC_PROP_2` decimal(13,4) default NULL,
+  `BOOL_PROP_1` varchar(1) default NULL,
+  `BOOL_PROP_2` varchar(1) default NULL,
+  PRIMARY KEY  (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  CONSTRAINT `qrtz_simprop_triggers_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`) REFERENCES `qrtz_triggers` (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `qrtz_triggers` */
+
+DROP TABLE IF EXISTS `qrtz_triggers`;
+
+CREATE TABLE `qrtz_triggers` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `JOB_NAME` varchar(200) NOT NULL,
+  `JOB_GROUP` varchar(200) NOT NULL,
+  `DESCRIPTION` varchar(250) default NULL,
+  `NEXT_FIRE_TIME` bigint(13) default NULL,
+  `PREV_FIRE_TIME` bigint(13) default NULL,
+  `PRIORITY` int(11) default NULL,
+  `TRIGGER_STATE` varchar(16) NOT NULL,
+  `TRIGGER_TYPE` varchar(8) NOT NULL,
+  `START_TIME` bigint(13) NOT NULL,
+  `END_TIME` bigint(13) default NULL,
+  `CALENDAR_NAME` varchar(200) default NULL,
+  `MISFIRE_INSTR` smallint(2) default NULL,
+  `JOB_DATA` blob,
+  PRIMARY KEY  (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  KEY `SCHED_NAME` (`SCHED_NAME`,`JOB_NAME`,`JOB_GROUP`),
+  CONSTRAINT `qrtz_triggers_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `JOB_NAME`, `JOB_GROUP`) REFERENCES `qrtz_job_details` (`SCHED_NAME`, `JOB_NAME`, `JOB_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ali_ath4_report` */
+
+DROP TABLE IF EXISTS `ali_ath4_report`;
+
+create view ali_ath4_report as
+select t.id,t.insertTime,t.businessMonth,t.businessDate,t.skillGroup,t.family,t.name,t.subject1,t.subject2,t.subject3,sum(t.count) totalCount,CAST(avg(t.duration) as decimal(38, 2)) avgDuration
+from ali_ath4_detail t group by t.subject3
+/*!50001 DROP VIEW IF EXISTS `ali_ath4_report` */;
+/*!50001 DROP TABLE IF EXISTS `ali_ath4_report` */;
+
+/*!50001 CREATE TABLE `ali_ath4_report` (
+  `id` int(11) NOT NULL default '0',
+  `insertTime` datetime default NULL,
+  `businessMonth` int(11) default NULL,
+  `businessDate` date default NULL,
+  `skillGroup` varchar(50) default NULL,
+  `family` varchar(50) default NULL,
+  `name` varchar(50) default NULL,
+  `subject1` varchar(50) default NULL,
+  `subject2` varchar(50) default NULL,
+  `subject3` varchar(50) default NULL,
+  `totalCount` decimal(32,0) default NULL,
+  `avgDuration` decimal(10,2) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 */;
+
+/*View structure for view ali_ath4_report */
+
+/*!50001 DROP TABLE IF EXISTS `ali_ath4_report` */;
+/*!50001 DROP VIEW IF EXISTS `ali_ath4_report` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `ali_ath4_report` AS select `t`.`id` AS `id`,`t`.`insertTime` AS `insertTime`,`t`.`businessMonth` AS `businessMonth`,`t`.`businessDate` AS `businessDate`,`t`.`skillGroup` AS `skillGroup`,`t`.`family` AS `family`,`t`.`name` AS `name`,`t`.`subject1` AS `subject1`,`t`.`subject2` AS `subject2`,`t`.`subject3` AS `subject3`,sum(`t`.`count`) AS `totalCount`,cast(avg(`t`.`duration`) as decimal) AS `avgDuration` from `ali_ath4_detail` `t` group by `t`.`subject3` */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
